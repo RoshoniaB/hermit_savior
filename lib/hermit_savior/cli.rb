1a548dee6984e50d5e18d24c539223c6920d4e34
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-29 07:01:39 +0000

    lib/cli.rb

--- lib/hermit_savior/cli.rb
+++ lib/hermit_savior/cli.rb
@@ -3,24 +3,10 @@ class HermitSavior::CLI
   def call 
     puts "Hey there hermit!!"
     quarantined
-    free_to_crawl
+    gives_option
   end
   
-  def gives_option
-    puts "Quarantined? or Free to crawl? or type 'X' to be a crabapple"
-   input = nil 
-    while input != "X"
-      input = gets.strip.downcase
-    case input
-    when "1" mv
-      puts "More info on option 1"
-    when "2"
-      puts "More info on option 2"
-      end  
-    end
-  end  
-    
-  def quarantined
+   def quarantined
     puts <<-DOC.gsub /^\s*/, ''
     1. Video
     2. Books
@@ -29,8 +15,8 @@ class HermitSavior::CLI
     5. Recipies
     DOC
   end
- 
-  def free_to_crawl
+  
+   def free_to_crawl
     puts <<-DOC.gsub /^\s*/, ''
     1. New Movies
     2. Biking
@@ -39,10 +25,23 @@ class HermitSavior::CLI
     5. Events
     DOC
   end
-  
+ 
+  def gives_option
+    puts "Quarantined? or Free to crawl? or type 'exit' to be a crabapple:"
+   input = nil 
+    while input != "exit"
+      input = gets.strip.downcase
+    case input
+    when "quarantined" 
+      puts "More info on option 1"
+    when "free_to_crawl"
+      puts "More info on option 2"
+      end  
+    end
+  end  
+ 
   def goodbye
     puts "Snap snap crabapple!!"
   end
-  
   
 end  
