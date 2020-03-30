b9655cbc85c2fed930abfd80fbf0fec2e770e9dc
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-30 16:20:20 +0000

    changed url in scraper

--- lib/hermit_savior/scraper.rb
+++ lib/hermit_savior/scraper.rb
@@ -1,17 +1,17 @@
 class Scraper
 
-BLOG_URL = "https://www.techradar.com/news/things-to-do"
+BLOG_URL = "https://www.psychologytoday.com/us/blog/intersections/202003/8-things-do-while-under-quarantine"
   
   def self.scrape_blog
     html = open(BLOG_URL)
     
     doc = Nokogiri::HTML(html)
-    
-     doc.css("#article-body").css("#1-watch-the-iheart-living-room-concert").each do |savior|
-  
+     
+     doc.css("ol")
+     binding.pry 
       to_do =
       url =
       Savior.new(to_do, url)
-    end
+    
   end
 end
