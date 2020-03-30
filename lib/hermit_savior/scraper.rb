2f9f0d894c6aa527cfdb881d54b894b8bfd1f9bd
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-30 06:21:23 +0000

    scraper is 1/2 working. Just need to add the rest of the data.

--- lib/hermit_savior/scraper.rb
+++ lib/hermit_savior/scraper.rb
@@ -1,25 +1,15 @@
 class Scraper
 
-BLOG_URL = "https://www.usatoday.com/story/life/health-wellness/2020/03/16/coronavirus-quarantine-100-things-do-while-trapped-inside/5054632002/"
+BLOG_URL = "https://www.techradar.com/news/things-to-do"
   
   def self.scrape_blog
     html = open(BLOG_URL)
+    
     doc = Nokogiri::HTML(html)
+    binding.pry
     
-    
-    doc.css(".gnt_ar_b").css(".gnt_ar_b_p").each do |savior|
-      binding.pry
-      group_1 =  
-      group_2 =
-      group_3 =
-      group_4 =  
-      group_5 =
-      group_6 =
-      group_7 =  
-      group_8 =
-      group_9 =
-      group_10 =
-      Savior.new(group_1, group_2, group_3, group_4, group_5, group_6, group_7, group_8, group_9, group_10)
-    end
+    doc.css("#article-body").css("#1-watch-the-iheart-living-room-concert")
+  
   end
+  
 end
