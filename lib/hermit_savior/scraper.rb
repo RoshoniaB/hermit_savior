00548fc832a0de03b5730aead03c7e60e7a7452d
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-31 23:01:00 +0000

    scraper add (sort of working)

--- lib/hermit_savior/scraper.rb
+++ lib/hermit_savior/scraper.rb
@@ -1,17 +1,18 @@
+
 class Scraper
 
-BLOG_URL = "https://www.psychologytoday.com/us/blog/intersections/202003/8-things-do-while-under-quarantine"
+  BLOG_URL = "https://www.psychologytoday.com/us/blog/intersections/202003/8-things-do-while-under-quarantine"
   
   def self.scrape_blog
     html = open(BLOG_URL)
     
     doc = Nokogiri::HTML(html)
-     
-     doc.css("ol")
-     binding.pry 
-      to_do =
-      url =
-      Savior.new(to_do, url)
+      
     
+    doc.css('ol').css('li').each do |savior|
+    binding.pry
+    todo =
+    url = 
+    Savior
   end
 end
